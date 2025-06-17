5927614f161d9bd7c985a634131619da46672d04
woahgrommit <02-crummy-trains@icloud.com>
2025-06-17 13:31:36 -0400

    Follower Count Reset

--- followersCount/followers_Count.js
+++ followersCount/followers_Count.js
@@ -6,6 +6,12 @@ function increaseCount() {
     checkCountValue(); // Check count value and display messages
 }
 
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Follower count reset to 0.")
+}
+
 function checkCountValue() {
     if (count === 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
